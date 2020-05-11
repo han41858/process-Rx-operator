fa17331f492782e52e5c1f54f0daa35564abe38c
han41858 <han41858@gmail.com>
2020-05-11 17:08:17 +0900

    combineLatest() test case added

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,7 +1,8 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import { from, Observer, of } from 'rxjs';
+import { combineLatest, from, Observer, of, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 describe('test', () => {
@@ -55,5 +56,58 @@ describe('observable', () => {
         };
 
         from(data).subscribe(observer);
+    });
+
+    it('combineLatest()', (done) => {
+        const delays: number[] = [
+            100,
+            200,
+            300
+        ];
+
+        const maxDelay: number = Math.max(...delays);
+        const delayMargin: number = 5;
+
+        let startTime: Date;
+        let timeGap: number;
+
+        let nextFncCallCount: number = 0;
+        let errorFncCallCount: number = 0;
+
+        const observer: Observer<number[]> = {
+            next(value: number[]): void {
+                expect(value).to.be.a('array');
+
+                timeGap = +new Date() - +startTime;
+
+                expect(timeGap).to.be.above(maxDelay - delayMargin);
+                expect(timeGap).to.be.below(maxDelay + delayMargin);
+
+                ++nextFncCallCount;
+            },
+            error(err: any): void {
+                ++errorFncCallCount;
+            },
+            complete(): void {
+                expect(nextFncCallCount).to.be.eql(1);
+                expect(errorFncCallCount).to.be.eql(0);
+
+                timeGap = +new Date() - +startTime;
+
+                expect(timeGap).to.be.above(maxDelay - delayMargin);
+                expect(timeGap).to.be.below(maxDelay + delayMargin);
+
+                done();
+            }
+        };
+
+        startTime = new Date();
+
+        combineLatest<number[]>(delays.map(value => {
+            return timer(value)
+                .pipe(
+                    map(() => value)
+                );
+        })).subscribe(observer);
     });
 });
